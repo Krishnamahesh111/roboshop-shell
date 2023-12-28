d55ecbb4a70a1fa1839af7a12447258715851789
Krishnamahesh111 <maheshsk828@gmail.com>
2023-12-28 18:40:51 +0530

    daws-76s

--- catalogue.sh
+++ catalogue.sh
@@ -94,9 +94,9 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB client"
 
-mongo --host $MONGODB_HOST </app/schema/catalogue.js &>> $LOGFILE
+mongo --host $MONGODB_HOST < /app/schema/catalogue.js &>> $LOGFILE
 
-VALIDATE $? "Loading catalouge data into MongoDB"
+VALIDATE $? "Loading catalogue data into MongoDB"
 
 
 
